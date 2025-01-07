3156dc06245e05ccf02a0111242bcdfd025be363
darthalex2014 <51307259+darthalex2014@users.noreply.github.com>
2025-01-07 18:00:12 +0500

    Update beam.config.ts

--- src/modules/beam/beam.config.ts
+++ src/modules/beam/beam.config.ts
@@ -15,9 +15,9 @@ export const SCATTER_COLOR = 'neutral' as const;
 export const SCATTER_DEBUG_STATE = false;
 export const SCATTER_PLACEHOLDER = '🖊️ ...'; // 💫 ..., 🖊️ ...
 export const SCATTER_RAY_DEF = 2;
-export const SCATTER_RAY_MAX = 8;
+export const SCATTER_RAY_MAX = 50;
 export const SCATTER_RAY_MIN = 1;
-export const SCATTER_RAY_PRESETS = [2, 4, 8];
+export const SCATTER_RAY_PRESETS = [2, 4, 8, 10, 20, 30, 40, 50];
 export const SCATTER_RAY_SHOW_DRAG_HANDLE = false;
 
 // configuration [BEAM Gather]
