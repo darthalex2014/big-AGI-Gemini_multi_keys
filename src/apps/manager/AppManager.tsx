5b9a8bc197eac4553185a1ab4207c189d3109ede
darthalex2014 <51307259+darthalex2014@users.noreply.github.com>
2025-01-21 18:36:56 +0500

    Create AppManager.tsx

---
+++ src/apps/manager/AppManager.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Box, Typography } from '@mui/joy';
+
+export function AppManager() {
+  return (
+    <Box sx={{ p: 2 }}>
+      <Typography level="h2">Manager</Typography>
+      <Typography level="body-sm">This is the Manager interface, work in progress</Typography>
+    </Box>
+  );
+}
